4fd0180cc95b7b92b9411a853748f533e98b147e
SeoSiun <siun0816@hanyang.ac.kr>
2022-01-11 13:33:39 +0900

    11729: 하노이의 탑
    System.out.println()으로 move를 매번 출력하면 시간초과
    -> String 객체를 하나 만들어서 +연산자로 연결해서 한 번 출력하게 바꿨지만 시간초과
    
    System.out.println()의 내부는 아래와 같이 동기화되어 있는데,
    public void println(String x) {
    	synchronized (this) {
    		print(x);
    		newLine();
    	}
    }
    이로 인해 오버헤드가 생겨서 성능이 저하될 수 있다.
    따라서 출력할 내용을 모아서 한 번의 System.out.println() 콜을 해야하는데,
    String은 불변(immutable) 객체이므로 이때 String 객체를 + 연산자로 연결하면 매번 새로운 객체를 만들게 되어 잦은 메모리 할당/해제를 발생시킨다.
    
    이런 문제를 해결하기 위해 StringBuilder를 사용할 수 있다.
    StringBuilder는 String과 문자열을 더할 때 새로운 객체를 생성하는것이 아니라 기존 데이터에 append하기 때문에 속도가 빠르고 부하가 적다.

---
+++ src/B11729.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B11729 {
+    static StringBuilder result = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        int n;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        System.out.println((1<<n)-1);
+        move(n,1,3);
+        System.out.println(result);
+    }
+
+    private static void move(int n, int s, int e){
+        if(n==1){
+            result.append(s+" "+e + "\n");
+            return;
+        }
+        // n-1개를 2번으로 옮김
+        move(n-1, s, 6-s-e);
+        // 가장 아래에 있는 애를 3번으로 옮김
+        result.append(s+" "+e + "\n");
+        // n-1개를 2에서 3으로 옮김
+        move(n-1, 6-s-e, e);
+    }
+}
