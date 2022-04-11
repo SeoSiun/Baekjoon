561b1b4c4b43e3e7f30388e1ef142dd17fa50d26
SeoSiun <siun0816@hanyang.ac.kr>
2022-04-11 19:26:40 +0900

    완주하지 못한 선수 - 해시
    [method1]
      실행은 되지만 효율성에서 0점.
      remove를 하면 array 전체를 돌기 때문에 시간이 오래 걸림.
    
    [method2]
      array 전체를 돌지 않기 위해, 우선 sort를 한 뒤 두 array가 달라지면 바로 리턴.
    
    [method3]
      HashMap 이용.
      각각의 participant를 key로 하여, 그 키가가 없다면 값을 1로, 이미 그 키가 존재한다면 그 값 + 1을 value로 해준다.
      그 뒤, 각각의 completion을 키로 하여, 그 키에 해당하는 vaule에 -1을 해준다.
      이 경우 어떤 선수가 완주했다면 그 선수의 이름을 key로 했을 때 해당하는 value는 0이어야 한다. 따라서 value가 0이 아닌 이름을 찾아 리턴해준다.

---
+++ Programmers/src/Level1/완주하지_못한_선수.java
@@ -0,0 +1,52 @@
+package Level1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class 완주하지_못한_선수 {
+    public static void main(String[] argv) {
+        System.out.println("Test1");
+        System.out.println(solution(new String[]{"leo", "kiki", "eden"}, new String[]{"eden", "kiki"}));
+        System.out.println("Test2");
+        System.out.println(solution(new String[]{"marina", "josipa", "nikola", "vinko", "filipa"}, new String[]{"josipa", "filipa", "marina", "nikola"}));
+        System.out.println("Test3");
+        System.out.println(solution(new String[]{"mislav", "stanko", "mislav", "ana"}, new String[]{"stanko", "ana", "mislav"}));
+    }
+
+    public static String solution(String[] participant, String[] completion) {
+        /***************** Method 1 ********************/
+//        String answer = "";
+//        ArrayList<String> _participant = new ArrayList<>(Arrays.asList(participant));
+//        for (String c: completion) {
+//            _participant.remove(c);
+//        }
+//        answer = _participant.get(0);
+//        return answer;
+
+        /***************** Method 2 ********************/
+//        int i;
+//
+//        Arrays.sort(participant);
+//        Arrays.sort(completion);
+//        for (i = 0; i < completion.length; i++) {
+//            if (!participant[i].equals(completion[i]))
+//                return (participant[i]);
+//        }
+//        return participant[i];
+
+        /***************** Method 3 ********************/
+        String answer = "";
+        HashMap<String, Integer> hm = new HashMap<>();
+        for(String p : participant) hm.put(p, hm.getOrDefault(p, 0) + 1);
+        for(String c : completion) hm.put(c, hm.get(c) - 1);
+
+        for (String key: hm.keySet()) {
+            if (hm.get(key) != 0) {
+                answer =  key;
+                break;
+            }m
+        }
+        return (answer);
+    }
+}
