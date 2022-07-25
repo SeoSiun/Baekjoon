487e9ab6ebed26d41a80d212e15adc28f4852155
SeoSiun <55352430+SeoSiun@users.noreply.github.com>
2022-07-25 21:01:25 +0900

    Time: 93 ms (20.35%), Space: 44.8 MB (72.34%) - LeetHub

--- LeetCode/1-two-sum/1-two-sum.java
+++ LeetCode/1-two-sum/1-two-sum.java
@@ -1,10 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] sol = new int[2];
+        int diff;
         
         for (int i = 0; i < nums.length; i++) {
+            diff = target - nums[i];
             for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
+                if (diff == nums[j]) {
                     sol[0] = i;
                     sol[1] = j;
                 }
