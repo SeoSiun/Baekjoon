cf8c75506eb1df4292bbc8ccde7ff05ab50d9420
SeoSiun <55352430+SeoSiun@users.noreply.github.com>
2022-07-28 02:02:53 +0900

    Time: 23 ms (67.58%), Space: 48.3 MB (76.34%) - LeetHub

---
+++ LeetCode/387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.lastIndexOf(s.charAt(i)) == i && s.indexOf(s.charAt(i)) == i)
+                return i;
+        }
+        return -1;
+    }
+}
