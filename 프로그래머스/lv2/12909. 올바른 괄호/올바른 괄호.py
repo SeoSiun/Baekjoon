5858c33b87c03c18779a21d5885796528b1a5bef
SeoSiun <55352430+SeoSiun@users.noreply.github.com>
2023-05-21 15:49:16 +0900

    [level 2] Title: 올바른 괄호, Time: 8.20 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,16 @@
+def solution(s):
+    cnt = 0
+    
+    for c in s:
+        if c == '(':
+            cnt += 1
+        if c == ')':
+            cnt -= 1
+            
+        if cnt < 0:
+            return False
+    
+    if cnt != 0:
+        return False
+    
+    return True
