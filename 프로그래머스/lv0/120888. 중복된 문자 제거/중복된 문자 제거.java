2d378e7db73d6417f780e59b0d7cf7585ce1639b
SeoSiun <55352430+SeoSiun@users.noreply.github.com>
2023-10-07 17:17:10 +0900

    [level 0] Title: 중복된 문자 제거, Time: 1.92 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        Set<Character> set = new LinkedHashSet<>();
+        for (char c: my_string.toCharArray()) {
+            set.add(c);
+        }
+        for(Character c: set) {
+            answer += c;
+        }
+        return answer;
+    }
+}
